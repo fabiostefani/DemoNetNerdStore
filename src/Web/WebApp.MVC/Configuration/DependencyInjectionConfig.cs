f3c2a984cfe158b670c192e67783fee587184729
Fabio de Stefani <fabiostefani@gmail.com>
2022-04-26 06:06:17 -0300

    Comunicação de API com Refit

--- src/Web/WebApp.MVC/Configuration/DependencyInjectionConfig.cs
+++ src/Web/WebApp.MVC/Configuration/DependencyInjectionConfig.cs
@@ -6,12 +6,19 @@ namespace WebApp.MVC.Configuration
 {
     public static class DependencyInjectionConfig
     {
-        public static void RegisterServices(this IServiceCollection services)
+        public static void RegisterServices(this IServiceCollection services, IConfiguration configuration)
         {
             services.AddTransient<HttpClientAuthorizationDelegationHandler>();
             services.AddHttpClient<IAutenticacaoService, AutenticacaoService>();
-            services.AddHttpClient<ICatalogoService, CatalogoService>()
-                .AddHttpMessageHandler<HttpClientAuthorizationDelegationHandler>();
+            
+            // services.AddHttpClient<ICatalogoService, CatalogoService>()
+            //     .AddHttpMessageHandler<HttpClientAuthorizationDelegationHandler>();
+            services.AddHttpClient("Refit", options =>
+                {
+                    options.BaseAddress = new Uri(configuration.GetSection("CatalogoUrl").Value);
+                })
+                .AddHttpMessageHandler<HttpClientAuthorizationDelegationHandler>()
+                .AddTypedClient(Refit.RestService.For<ICatalogoServiceRefit>);
             services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
             services.AddScoped<IUser, AspNetUser>();
         }
---
+++ src/Web/WebApp.MVC/Services/ICatalogoServiceRefit.cs
@@ -0,0 +1,13 @@
+﻿using Refit;
+using WebApp.MVC.Models;
+
+namespace WebApp.MVC.Services;
+
+public interface ICatalogoServiceRefit
+{
+    [Get("/catalogo/produtos")]
+    Task<IEnumerable<ProdutoViewModel>> ObterTodos();
+    
+    [Get("/catalogo/produtos/{id}")]
+    Task<ProdutoViewModel> ObterPorId(Guid id);
+}
