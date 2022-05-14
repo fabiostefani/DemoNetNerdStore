dc8b31cf25e341415e8704057fbe32f2960ad9d8
Fabio de Stefani <fabiostefani@gmail.com>
2022-05-14 06:25:47 -0300

    Inicio do Carrinho

--- src/Services/Carrinho/Carrinho.API/Controllers/CarrinhoController.cs
+++ src/Services/Carrinho/Carrinho.API/Controllers/CarrinhoController.cs
@@ -1,8 +1,35 @@
-﻿using Api.Core.Controllers;
+﻿using System.Threading.Tasks;
+using System.Security.Cryptography.X509Certificates;
+using Api.Core.Controllers;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Carrinho.API.Model;
 
 namespace Carrinho.API.Controllers;
 
+[Authorize]
 public class CarrinhoController : MainController
 {
-    
+    [HttpGet("carrinho")]
+    public async Task<CarrinhoCliente> ObterCarrinho()
+    {
+        return null;
+    }
+    [HttpPost("carrinho")]
+    public async Task<IActionResult> AdicionarItemCarrinho(Carrinho item)
+    {
+        return CustomResponse();
+    }
+
+    [HttpPut("carrinho/{produtoId}")]
+    public async Task<IActionResult> AtualizarItemCarrinho(Guid produtoId, CarrinhoItem item)
+    {
+        return CustomResponse();
+    }
+
+    [HttpDelete("carrinho/{produtoId}")]
+    public async Task<IActionResult> RemoverItemCarrinho(Guid produtoId)
+    {
+        return CustomResponse();
+    }
 }
