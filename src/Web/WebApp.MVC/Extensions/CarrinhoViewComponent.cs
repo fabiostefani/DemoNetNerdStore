efbf49a4082c5791d3123411c82594b3bd842112
Fabio de Stefani <fabiostefani@gmail.com>
2022-05-23 06:06:10 -0300

    Feat: Finalização de Carrinho de Compras

---
+++ src/Web/WebApp.MVC/Extensions/CarrinhoViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApp.MVC.Models;
+using WebApp.MVC.Services;
+
+namespace WebApp.MVC.Extensions;
+
+public class CarrinhoViewComponent : ViewComponent
+{
+    private readonly ICarrinhoService _carrinhoService;
+
+    public CarrinhoViewComponent(ICarrinhoService carrinhoService)
+    {
+        _carrinhoService = carrinhoService;
+    }
+
+    public async Task<IViewComponentResult> InvokeAsync()
+    {
+        return View(await _carrinhoService.ObterCarrinho() ?? new CarrinhoViewModel());
+    }
+}
