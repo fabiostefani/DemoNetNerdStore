e3fb860e7b125c419e91f08a1493e0a8d184cc13
Fabio de Stefani <fabiostefani@gmail.com>
2022-07-04 06:39:39 -0300

    feat: Início do processo de pagamento

---
+++ src/Services/Pagamento/Pagamentos.NerdsPag/NerdsPagService.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace Pagamentos.NerdsPag;
+
+public class NerdsPagService
+{
+    public readonly string ApiKey;
+    public readonly string EncryptionKey;
+
+    public NerdsPagService(string apiKey, string encryptionKey)
+    {
+        ApiKey = apiKey;
+        EncryptionKey = encryptionKey;
+    }
+}
---
+++ src/Services/Pagamento/Pagamentos.NerdsPag/PaymentMethod.cs
@@ -0,0 +1,7 @@
+﻿namespace Pagamentos.NerdsPag;
+
+public enum PaymentMethod
+{
+    CreditCard = 1,
+    Billet
+}
